facef6f57efbdb5c035659b7b3b45f706805596a
Rahul Das <iamrahuldas001@gmail.com>
2021-02-07 21:42:47 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+// npm i redux react-redux redux-thunk redux-devtools-extension
+import React from 'react';
+import './App.css';
+const App = () => {
+  return (
+    <div>
+      <h1>REACT REDUX STARTER PACK</h1>
+    </div>
+  );
+};
+
+export default App;
